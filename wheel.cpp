eb91a02254772ba74e1bf0a747c5dc2c6232a338
Sol Boucher <slb1566@rit.edu>
2014-03-02 07:16:31 -0500

    Expose path mode UI in wheel application

--- wheel.cpp
+++ wheel.cpp
@@ -2,17 +2,30 @@
 // CSC-541-01 Strout's F(ant)abulous C++11 Concoction
 // 03/05/14 Project 1
 
+#include <cstring>
 #include <iostream>
 #include "solver.h"
 #include "WheelConfig.h"
 using std::cin;
+using std::cerr;
 using std::cout;
 using std::endl;
+using std::forward_list;
 using std::make_shared;
 using std::shared_ptr;
 using std::vector;
 
-int main() {
+int main(int argc, char *argv[]) {
+	bool pathMode = false;
+	if(argc > 1) {
+		if(argc == 2 && !strcmp(argv[1], "path"))
+			pathMode = true;
+		else {
+			cerr << "Usage: wheel {path}" << endl;
+			return 1;
+		}
+	}
+
 	unsigned triads_count;
 	cin >> triads_count;
 
@@ -37,7 +50,13 @@ int main() {
 	shared_ptr<Configuration> puzzle_state =
 			make_shared<WheelConfig>(triads_count,
 					move(connector_sums), move(starting_config));
-	puzzle_state = solver(puzzle_state);
+	shared_ptr<forward_list<shared_ptr<Configuration>>> puzzle_hist =
+			make_shared<forward_list<shared_ptr<Configuration>>>();
 
+	puzzle_state = pathMode ?
+			solver(puzzle_state, puzzle_hist) : solver(puzzle_state);
+
+	for(shared_ptr<Configuration> snapshot : *puzzle_hist)
+		cout << *snapshot << endl;
 	cout << (puzzle_state ? *puzzle_state : "No solution!") << endl;
 }
